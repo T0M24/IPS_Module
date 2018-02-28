ea874111da024f7b59ad51a51f259f863b1c2932
T0M24 <thomas.gierth@gmx.de>
2018-02-28 23:42:45 +0100

    fix

--- CTRL_PT1/module.php
+++ CTRL_PT1/module.php
@@ -162,12 +162,12 @@
             //zuerst die Reglervariablen ermitteln
             
             
-            $In      = $this->GetValueFloat($this->GetIDForIdent ( self::IDENT_IN));
-            $In_old  = $this->GetValueFloat($In_old_ID = $this->GetIDForIdent ( self::IDENT_INOLD));
+            $In      = GetValueFloat($this->GetIDForIdent ( self::IDENT_IN));
+            $In_old  = GetValueFloat($In_old_ID = $this->GetIDForIdent ( self::IDENT_INOLD));
             
-            $a0   = $this->GetValueFloat($this->GetIDForIdent( self::IDPROP_A0));
-            $a1   = $this->GetValueFloat($this->GetIDForIdent( self::IDPROP_A1));
-            $b1   = $this->GetValueFloat($this->GetIDForIdent( self::IDPROP_B1));
+            $a0   = GetValueFloat($this->GetIDForIdent( self::IDPROP_A0));
+            $a1   = GetValueFloat($this->GetIDForIdent( self::IDPROP_A1));
+            $b1   = GetValueFloat($this->GetIDForIdent( self::IDPROP_B1));
             
             $out=$In_old + $a0*$In;
             $In_old=$a1*$In - $b1*$out;
